3967a827301dcc473fbb077eb9db2c552a43d6a4
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-10 16:45:52 +0545

    fix: getData with api change to env URL

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
     cache: "no-store",
   });
 
