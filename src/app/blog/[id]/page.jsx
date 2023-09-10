e0519a510fd8719ce6f3c48aefdfdef446382b21
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-10 16:46:09 +0545

    fix: getDatabyId with api change to env URL

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/${id}`, {
     cache: "no-store",
   });
 
